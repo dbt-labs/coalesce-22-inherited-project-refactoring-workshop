5a512205d0e4c1e8f71afc4ac4a9ffc4a7aaba37
Christine <christinebergersd@gmail.com>
2022-09-20 11:37:55 -0500

    Added second exercise for auditing

---
+++ _resources/auditing_code/exercise_2/audit_compare_queries__eu_lowcost_brass_suppliers.sql
@@ -0,0 +1,26 @@
+{% set old_query %}
+  select * from {{ ref('EUR_LOWCOST_BRASS_SUPPLIERS') }}
+{% endset %}
+
+{% set new_query %}
+  select
+    part_name as p_name, 
+    part_size as p_size, 
+    part_retail_price as p_retailprice, 
+    supplier_account_balance as s_acctbal, 
+    supplier_name as s_name, 
+    nation as n_name, 
+    part_id as p_partkey, 
+    part_manufacturer as p_mfgr, 
+    supplier_address as s_address, 
+    supplier_phone_number as s_phone, 
+    supplier_comment as s_comment
+  from {{ ref('report__eu_lowcost_brass_suppliers_size_15') }}
+{% endset %}
+
+{{ audit_helper.compare_queries(
+    a_query=old_query,
+    b_query=new_query,
+    primary_key="supplier_name || part_id",
+    summarize=false
+) }}
