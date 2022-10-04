b0b35d5d28277d09e5d411fe6377fd7039cc5da2
Christine <christinebergersd@gmail.com>
2022-10-03 19:32:57 -0500

    Removed other examples - those will be on another branch for async participation

--- _resources/auditing_code/exercise_4/audit_tests__eu_lowcost_brass_suppliers.sql
+++
@@ -1,9 +0,0 @@
-{% set old_etl_relation=ref('EUR_LOWCOST_BRASS_SUPPLIERS') -%}
-{% set dbt_relation=ref('report__eu_lowcost_brass_suppliers_size_15') %}
-
-{{ audit_helper.compare_relations(
-    a_relation=old_etl_relation,
-    b_relation=dbt_relation,
-    primary_key="s_name || p_partkey",
-    summarize=false
-) }}
